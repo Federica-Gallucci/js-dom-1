d6cc5aeff78cb82d278c5a3bddd68fc4007f435b
Federica-Gallucci <gallucci.federica.c@gmail.com>
2025-04-09 16:21:13 +0200

    add yellow lamp

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log("ciao");
+// **Consegna
+// Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
+// Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
+
+// **BONUS
+// Facciamo accendi e spegni:
+// Al primo click la lampadina si accende e nel bottone compare la scritta "Spegni"
+// Al secondo click la lampadina si spegne e nel bottone compare la scritta "Accendi"
+// E così via...
+// Per fare questo bonus potremmo aver bisogno di del metodo string.includes()
+
+const lampEl = document.querySelector(".lamp-white");
+const buttonEl = document.getElementById("button-on");
+// const lampYellow = document.querySelector(".lamp-yellow");
+// const buttonOff = document.getElementById("button-off");
+
+console.log(lampEl);
+console.log(buttonEl);
+// console.log(lampYellow);
+// console.log(buttonOff);
+
+buttonEl.addEventListener("click", function () {
+  lampEl.src = `./img/yellow_lamp.png`;
+});
